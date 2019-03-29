af0f81f8c8fdce69fc4fefe59f044599935132ab
Hannah Hoff <hannahhoff@Hannahs-MacBook-Pro.local>
2019-03-29 09:06:05 -0600

    initial commit

---
+++ MovieC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieC
+//
+//  Created by Hannah Hoff on 3/29/19.
+//  Copyright © 2019 Hannah Hoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MovieC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MovieC
+//
+//  Created by Hannah Hoff on 3/29/19.
+//  Copyright © 2019 Hannah Hoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
